dece413fc0d11863a8c1208ca31cb0a123b69813
Chelsea Griffo <chelseagriffo@gmail.com>
2023-02-04 16:47:15 -0600

    Added start_game method to game.py

--- game.py
+++ game.py
@@ -9,6 +9,22 @@ class Game():
         self.active_phrase = self.get_random_phrase()
         self.guesses = [" "]
 
+    def start_game(self):
+        self.welcome()
+        print(f"Number missed: {self.missed}")
+        self.active_phrase.display(self.guesses)
+
+
+    def welcome(self):
+        print('''
+        
+        ================================================================================\n
+        Hey there heeyyy movie buff! Are you ready to guess some epic cinematic quotes?!\n
+        ================================================================================
+        
+        
+        ''')
+
     def create_phrases(self): 
         phrases = [Phrase("Riddles in the dark"), Phrase("The wand chooses the wizard"), 
                         Phrase("The bog of eternal stench"), Phrase("Anybody want a peanut"), 
